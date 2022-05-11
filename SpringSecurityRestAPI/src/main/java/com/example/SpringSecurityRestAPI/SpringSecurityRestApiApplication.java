237b28fd6b74be99936ef8a878c0221f1f85a134
HaDucDev <haductrung392000@gmail.com>
2022-05-11 20:05:09 +0700

    first

---
+++ SpringSecurityRestAPI/src/main/java/com/example/SpringSecurityRestAPI/SpringSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRestApiApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityRestAPI/src/test/java/com/example/SpringSecurityRestAPI/SpringSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
